f8cae188162aa2a9303f5205baf94c8a7ee84310
Nisio110 <oisinlane05@gmail.com>
2026-02-18 14:47:21 +0000

    created participant class

--- include/Participants.h
+++ include/Participants.h
@@ -8,7 +8,18 @@ using std::vector;
 class Participants{
     int id{-1};
     vector<string> names;
+public :
+    // Getters
+    int getID();
+    vector<string> getNames();
 
+    // Setters
+    void setID(int id);
+    void setNames(vector<string> names);
+
+    // General
+    void addName(string name);
+    void removeName(int pid);
 };
 
 #endif

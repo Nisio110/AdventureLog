b7acc05433850014738edab74419630b4d7d4793
Nisio110 <oisinlane05@gmail.com>
2026-02-18 14:30:54 +0000

    i didnt make these changes in main i swear

---
+++ include/Participants.h
@@ -0,0 +1,14 @@
+#ifndef _PARTICIPANTS_
+#define _PARTICIPANTS_
+#include <vector>
+#include <string>
+using std::string;
+using std::vector;
+
+class Participants{
+    int id{-1};
+    vector<string> names;
+
+};
+
+#endif

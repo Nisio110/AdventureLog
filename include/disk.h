215c22d5419444770645a566c842b41c9200702a
Nisio110 <oisinlane05@gmail.com>
2026-03-06 10:43:27 +0000

    whatever i did last night

--- include/disk.h
+++ include/disk.h
@@ -7,8 +7,8 @@ class Log;
 class Participant;
 
 class Disk{
-    const std::string filename{"disk.yaml"};
-    std::ifstream* diskFile;
+    static const std::string filename;
+    static std::ifstream diskFile;
     int numObjects{}, numUsers{}, numLogs{}, numParticipants{};
     // vectors contain start and end lines for each object in the file.
     std::vector<int> userLocations{}, logLocations{}, participantLocations{}; 
@@ -17,8 +17,8 @@ class Disk{
     // TODO: sort each object type into its own section    
 public:
     // Getters
-    std::vector<int> getFileInfo();
-    std::ifstream* getDiskFile();
+    const std::vector<int>& getFileInfo();
+    std::ifstream& getDiskFile();
 
     // Setters
     // Decided against setters for now.
@@ -30,10 +30,10 @@ public:
     // Read Operations
     void openDisk();
     bool isFileGood();
-    std::vector<int>* readFileInfo(); // will write to fileInfo vector
-    std::vector<User>* loadUsers(); 
-    std::vector<Log>* loadLogs(); // will be called once the user has been selected. will load appropriate logs for the user
-    std::vector<Participant>* loadParticipants(); // will be called inside loadLogs()
+    std::vector<int>& readFileInfo(); // will write to fileInfo vector
+    std::vector<User>& loadUsers(); 
+    std::vector<Log>& loadLogs(); // will be called once the user has been selected. will load appropriate logs for the user
+    std::vector<Participant>& loadParticipants(); // will be called inside loadLogs()
 
 };
 

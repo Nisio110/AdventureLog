09be3ee45ae178a488849ec8a87415300c93c96f
Nisio110 <oisinlane05@gmail.com>
2026-03-28 17:06:31 +0000

    feat: starting work on dynamic initialisation

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,33 +4,9 @@
 int main(){
 	Disk d;
 	d.initProgram();
-	d.printUserDetails();
-
-	/*
-	std::vector<std::pair<std::string, std::string>> v {d.parseFile()};
-	for (auto pair : v){
-		std::cout << std::get<0>(pair) << " -> " << std::get<1>(pair) << "\n";
-	}
-	*/
-	//size_t i{3};
-	//d.parseStr(i);
-	
-	//d.parseObjectBody("User",1);
-	
-	/*
-	std::string targetAttr {"id"};
-	std::string targetValue("1");
-	std::vector<size_t> attrLocs = d.parseAttrLocs(targetAttr);
-	for (int i{};i<size(attrLocs);++i)
-		std::cout << attrLocs[i]+1 << "\n";
-	*/
-	/*
-	du.parseObjectLocs();
-	for (int i{};i<size(du.getObjectLocs()); ++i)
-		std::cout << du.getObjectLocs()[i] << std::endl;
-	*/
-	return 0;
+	//d.printUserDetails();
 }
+
 
 void basicTest(){
 	User u1("oisin", "password");

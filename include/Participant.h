3e792a55cfdbc448f51b77042217a53ef0c0835d
Nisio110 <laneoisin64@gmail.com>
2026-04-09 17:43:14 +0100

    Participants are being added to the log initialisation process
    Users will soon have the same done it
    Owner id variable renamed to LogID for clarity

--- include/Participant.h
+++ include/Participant.h
@@ -5,22 +5,30 @@
 class Participant{
     std::string name;
     int id;
+    int logID{0};
     static int numParticipants;
 
     int generateID(); // private function
 public :
     // Getters
     int getID();
+    inline int getLogID() {return logID;}
     std::string getName();
     int getNumParticipants();
 
+    // General
+    void print();
+
     // Setters
     void setID(int id);
+    inline void setLogID(int id) { logID = id;}
     void setName(std::string names);
     void setNumParticipants(int num);
 
     // Constructors
-    Participant(std::string name);
+    Participant(std::string _name);
+    Participant(std::string _name, int _logID);
+    Participant(std::string _name, int _logID, int _id);
 };
 
 namespace ParticipantTests {
--- src/Participant.cpp
+++ src/Participant.cpp
@@ -16,6 +16,14 @@ void Participant::setName(string n){ name = n; }
 void Participant::setNumParticipants(int num){ numParticipants = num; }
 
 // General
+void Participant::print(){
+    std::cout << "= Printing participant details ===\n"
+              << " ID: " << getID() << "\n"
+              << " Name: " << getName() << "\n"
+              << "==================================\n"
+              << "\n";
+}
+
 int Participant::generateID(){
     setNumParticipants(numParticipants+1);
     id = getNumParticipants();
@@ -23,9 +31,20 @@ int Participant::generateID(){
 }
 
 // Constructors
-Participant::Participant(string name){
+Participant::Participant(std::string name){
     setName(name);
     generateID();
+}
+Participant::Participant(std::string name, int logID){
+    setName(name);
+    setLogID(logID);
+    generateID();
+}
+
+Participant::Participant(std::string name, int logID, int id){
+    setName(name);
+    setLogID(logID);
+    setID(id);
 }
 
 // Tests

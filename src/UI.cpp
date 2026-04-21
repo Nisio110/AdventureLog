9ab2803741a37d0e5ef8ab066bf1fb166ce64d54
Torterra31205 <c24350546@mytudublin.ie>
2026-04-21 10:34:37 +0100

    ui functions

--- src/UI.cpp
+++ src/UI.cpp
@@ -1,37 +1,35 @@
 //#include "../include/DiskUsers.h"
+#include "../include/State.h"
 #include <iostream>
 #include "../include/UI.h"
 using namespace std;
 
 int ui(){
+    State s;
+    s.initProgram();
+    std::vector<User*> users = s.getUsers();
     int choice=-1;
+    int log=-1;
+    int page=1;
     bool loop=1;
     int menu=0;
     while (loop==1) {
         switch (menu) {
             case 0:
-                cout << "Welcome to the Adventure Log System" << endl 
-                     << "Please choose an option" << endl
-                     << "1: Log in" << endl
-                     << "2: Sign up" << endl
-                     << "3: Exit" << endl;
-                cin >> choice;
+                choice = startupMenu();
                 switch (choice){
                     case 1:
-                        cout << "Please enter your username: ";
-                        cout << endl;
-                        cout << "Please enter your password: ";
-                        cout << endl;
-                        menu=1;
+                        if (logIn()) { //some bool stuff here will need tweaks when the actual loginchecks are implemented
+                            menu=1;
+                        }
+                        else{menu=0;}
                         break;
                     case 2:
-                        cout << "Please choose a username: ";
-                        cout << endl;
-                        cout << "Please choose a password: ";
-                        cout << endl;
+                        signUp();
                         menu=1;
                         break;
                     case 3:
+                        s.save();
                         return 0;
                         break;
                     default:
@@ -40,11 +38,7 @@ int ui(){
                 break;
     
             case 1:
-                cout << "Main Menu" << endl
-                     << "1: View Logs" << endl
-                     << "2: Add Logs" << endl
-                     << "3: Logout" << endl;
-                cin >> choice;
+                choice = mainMenu();
                 switch(choice){
                     case 1: menu=2;
                     case 2: menu=3;
@@ -53,7 +47,22 @@ int ui(){
                 }
                 break;
             case 2:
-                cout << endl;
+                choice = logMenu(page);
+                log=choice;
+                switch(choice){
+                    case 1: loadLog(log);
+                    case 2: loadLog(log);
+                    case 3: loadLog(log);
+                    case 4: loadLog(log);
+                    case 5: loadLog(log);
+                    case 6: loadLog(log);
+                    case 7: page++;
+                    case 8: if (page>1) {page--;}
+                    case 9: menu=1;
+                    case 0: sortLogs();
+                    default:cout << "**Invalid Input: Please choose a valid option**" << endl;
+                }
+                break;
 
 
         }

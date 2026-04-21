685871d6660e296378b9f7e7427fcd7808a3bd99
Nisio110 <oisinlane05@gmail.com>
2026-04-21 15:40:47 +0100

    patch(ui): Refactored ui so that definitions are in the cpp file rather than the header file

--- include/UI.h
+++ include/UI.h
@@ -1,89 +1,22 @@
 #ifndef UI_H
 #define UI_H
+
 int ui();
-
-int startupMenu()
-{
-    int choice;
-    cout << "Welcome to the Adventure Log System" << endl 
-         << "Please choose an option" << endl
-         << "1: Log in" << endl
-         << "2: Sign up" << endl
-         << "3: Exit" << endl
-         << "Select Option: ";
-    cin >> choice;
-    return choice;
-};
-
-bool logIn()
-{
-    cout << "Please enter your username: ";
-    cout << endl;
-    cout << "Please enter your password: ";
-    cout << endl;
-    return true;
-}
-
-void signUp()
-{
-    logIn(); //disk stuff that need to be done
-}
-
-int mainMenu()
-{
-    int choice;
-    cout << "Main Menu" << endl
-         << "1: View Logs" << endl
-         << "2: Add Logs" << endl
-         << "3: User Settings" << endl
-         << "4: Logout" << endl
-         << "Select Option: ";
-    cin >> choice;
-    return choice;
-}
-
-int logMenu(int page)
-{
-    int choice;
-    int page=page;
-
-    cout << "Page: " << page << endl;
-}
-
-void loadLog(int log){}
-
-void sortLogs(){}
-
-void editLog(){}
-
-void deleteLog(){}
-
-void addLog(){}
-
-int userSettings()
-{
-    int choice;
-    cout << "User Settings" << endl
-         << "1: Change Username" << endl
-         << "2: Change Password" << endl
-         << "3: Delete Account" << endl
-         << "4: Back to Main Menu" << endl
-         << "Select Option: ";
-    cin >> choice;
-    return choice;
-}
-
-void changeUsername(){}
-
-void changePassword(){}
-
-void deleteAccount(){}
-
-
-
-
+int startupMenu();
+bool logIn();
+void signUp();
+int mainMenu();
+int logMenu(int page);
+void loadLog(int log);
+void sortLogs();
+void editLog();
+void deleteLog();
+void addLog();
+int userSettings();
+void changeUsername();
+void changePassword();
+void deleteAccount();
 vector<Log*> sortID(vector<Log*> logs);
 vector<Log*> sortDuration(vector<Log*> logs);
-
 
 #endif
--- src/UI.cpp
+++ src/UI.cpp
@@ -71,5 +71,63 @@ int ui(){
     return 0;
 }
 
+int startupMenu()
+{
+    int choice;
+    cout << "Welcome to the Adventure Log System" << endl 
+         << "Please choose an option" << endl
+         << "1: Log in" << endl
+         << "2: Sign up" << endl
+         << "3: Exit" << endl
+         << "Select Option: ";
+    cin >> choice;
+    return choice;
+};
 
+bool logIn()
+{
+    cout << "Please enter your username: ";
+    cout << endl;
+    cout << "Please enter your password: ";
+    cout << endl;
+    return true;
+}
 
+void signUp()
+{
+    logIn(); //disk stuff that need to be done
+}
+
+int mainMenu()
+{
+    int choice;
+    cout << "Main Menu" << endl
+         << "1: View Logs" << endl
+         << "2: Add Logs" << endl
+         << "3: User Settings" << endl
+         << "4: Logout" << endl
+         << "Select Option: ";
+    cin >> choice;
+    return choice;
+}
+
+int logMenu(int page)
+{
+    int choice;
+    int page=page;
+
+    cout << "Page: " << page << endl;
+}
+
+int userSettings()
+{
+    int choice;
+    cout << "User Settings" << endl
+         << "1: Change Username" << endl
+         << "2: Change Password" << endl
+         << "3: Delete Account" << endl
+         << "4: Back to Main Menu" << endl
+         << "Select Option: ";
+    cin >> choice;
+    return choice;
+}
